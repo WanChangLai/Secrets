9c917c7ce559528879cb087c03db829c22dae117
Wan Chang Lai <wanchang.lai@live.com>
2022-10-22 22:18:29 +0800

    Complete Level 3 -- Hashing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,10 +25,6 @@ const userSchema = new mongoose.Schema({
   email:String,
   password:String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 
 const User = mongoose.model('User', userSchema);
 
@@ -48,7 +44,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -63,7 +59,7 @@ app.post("/register",function(req,res){
 //check whether the user is already in our databases
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if(err){
