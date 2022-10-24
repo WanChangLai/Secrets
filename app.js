244500535e5bf83f9a73b86bffe25aac1c44a023
Wan Chang Lai <wanchang.lai@live.com>
2022-10-24 16:49:33 +0800

    Final Version -- Allow Users to Submit a Secret

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true});
 const userSchema = new mongoose.Schema({
   email:String,
   password:String,
-  googleId:String
+  googleId:String,
+  secret:String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -96,14 +97,48 @@ app.get("/register",function(req,res){
   res.render("register")
 });
 
-//if a ppl is already log in, he shoud be able to view this page
+//find all the users with secret in the collection
 app.get("/secrets",function(req,res){
+  User.find({"secret":{$ne:null}},function(err,foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if (foundUsers){
+        res.render("secrets",{usersWithSecrets:foundUsers})
+      }
+    }
+  });
+});
+
+app.get("/submit",function(req,res){
+  //to check whether the authentication is done
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
 });
+
+app.post("/submit",function(req,res){
+  const submittedSecret = req.body.secret
+
+  //console.log(req.user.id);
+
+  User.findById(req.user.id,function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
+
 
 app.get("/logout",function(req,res){
   req.logout(function(err){
